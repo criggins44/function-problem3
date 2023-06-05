e03e0e9f67af6a22abee108d9aff67926523872e
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-06-05 15:01:30 -0400

    Added comments to explain logic

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 countLetter = (str, letter) => {
+//used arrow function to define the parameters
+
 	let counter = 0
 	let array = str.split("")
-	
+//array splits the string into an array of letters
+
 	for(let i = 0; i < array.length; i++) {
 		if (array[i] == letter) {
 			counter++;
 		}
 	} return counter
-
+//loops through each index of the array checking for the specified letter
+//each time the letter occurs, the counter increases by one
 }
+
 console.log(countLetter('Very cold outside', 'O'));
 
