e66d169e00a9955227d510554da5fb64f93b191d
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-06-07 13:18:24 -0400

    Converted array elements and parameter to lower case when they are compared

--- script.js
+++ script.js
@@ -6,13 +6,14 @@ countLetter = (str, letter) => {
 //array splits the string into an array of letters
 
 	for(let i = 0; i < array.length; i++) {
-		if (array[i] == letter) {
+		if (array[i].toLowerCase() === letter.toLowerCase()) {
 			counter++;
 		}
 	} return counter
 //loops through each index of the array checking for the specified letter
+//the letter in the array and the letter as a parameter are both compared as lower case elements
 //each time the letter occurs, the counter increases by one
 }
 
-console.log(countLetter('Very cold outside', 'O'));
+console.log(countLetter('Our cold outside', 'O'));
 
